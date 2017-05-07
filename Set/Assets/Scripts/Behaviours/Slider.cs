280f2e803464b9f7c732e8e465b64e02cf1e28f7
Rafaela9136 <rafaela9136@gmail.com>
2017-05-07 01:56:26 -0300

    Adding market scenes

---
+++ Set/Assets/Scripts/Behaviours/Slider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Slider : MonoBehaviour {
+
+	public Text actualValue;
+	
+	// Update is called once per frame
+	void Update () {
+		float sliderValue = this.GetComponent<UnityEngine.UI.Slider>().value;
+
+		actualValue.text = string.Format("{0:N1}", sliderValue);
+	}
+}
